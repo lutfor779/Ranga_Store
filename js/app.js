683577054ac89963af52074e545a82bb1e82015d
lutfor779 <mdlrrahman779@gmail.com>
2021-09-13 21:48:34 +0600

    almost ok

--- js/app.js
+++ js/app.js
@@ -2,31 +2,31 @@ const loadProducts = () => {
   const url = `https://fakestoreapi.com/products`;
   fetch(url)
     .then((response) => response.json())
-    .then((data) => showProducts(data));
+    .then((data) => productDetailss(data));
 };
 loadProducts();
 
 // show all product in UI 
-const showProducts = (products) => {
+const productDetailss = (products) => {
   const allProducts = products.map((pd) => pd);
   for (const product of allProducts) {
     const image = product.image;
     const div = document.createElement("div");
     div.classList.add("product");
-    div.innerHTML = `<div class="single-product">
+    div.innerHTML = `<div class="single-product" style="background-color: ${randomColor()}">
       <div>
-    <img class="product-image" src=${image}></img>
+    <img class="products-image" src=${image}></img>
       </div>
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
-      <p>Total Rate: ${product.rating.count}</p>
-      <p>Rating: ${product.rating.rate}</p>
+      <p>Total rated: <span class="text-primary">${product.rating.count}</span></p>
+      <p>Rating: <span class="text-danger">${product.rating.rate}</span></p>
       <h2>Price: $ ${product.price}</h2>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
+      <a href="#"><button id="details-btn" onclick="product(${product.id})" class="btn btn-danger">Details</button></a></div>
       `;
-
     document.getElementById("all-products").appendChild(div);
+    
   }
 };
 let count = 0;
@@ -81,3 +81,52 @@ const updateTotal = () => {
     getInputValue("total-tax");
   document.getElementById("total").innerText = grandTotal.toFixed(2);
 };
+
+
+// product detail
+const product = id => {
+  const url = `https://fakestoreapi.com/products/${id}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => productDetails(data));
+}
+const productDetails = product => {
+  document.getElementById("show-product").textContent = '';
+  const image = product.image;
+  const div = document.createElement("div");
+  // div.classList.add("product");
+  div.innerHTML = `<div class="single-product">
+      <div>
+    <img class="product-image" src=${image}></img>
+      </div>
+      <h3>${product.title}</h3>
+      <p>Category: ${product.category}</p>
+      <span id="stars">a</span><br>
+      <span>${product.rating.count}</span>
+      <h2>Price: $ ${product.price}</h2>
+      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
+      </div>
+      `;
+  document.getElementById("show-product").appendChild(div);
+  document.getElementById("stars").innerHTML = getStars(product.rating.rate);
+}
+
+function getStars(rate) {
+  // Round to nearest half
+  rate = Math.round(rate);
+  let output = [];
+
+  // Append all the filled whole stars
+  for (var i = rate; i >= 1; i--)
+    output.push('<i class="fa fa-star" aria-hidden="true" style="color: gold;"></i>&nbsp;');
+
+  // If there is a half a star, append it
+  if (i == .5) output.push('<i class="fa fa-star-half-o" aria-hidden="true" style="color: gold;"></i>&nbsp;');
+
+  // Fill the empty stars
+  for (let i = (5 - rate); i >= 1; i--)
+    output.push('<i class="fa fa-star-o" aria-hidden="true" style="color: gold;"></i>&nbsp;');
+  return output.join('');
+}
+
+const randomColor = () => '#' + (0x1000000 + Math.random() * 0xffffff).toString(16).substr(1, 6) + 15;
